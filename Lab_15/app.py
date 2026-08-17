c87e92c22f84d41852140326e302f45a6cc4ad62
Rahuldee <Rahuldee@gmail.com>
2026-08-17 14:47:08 -0700

    Edge AI 3.0 labs
    Jupyter notebook based labs

--- Lab_15/app.py
+++
@@ -1,9 +0,0 @@
-
-import streamlit as st
-
-st.title("Simple CoCalc Streamlit App")
-st.write("Hello! This app is running on CoCalc.")
-
-name = st.text_input("What is your name?")
-if name:
-    st.write(f"Nice to meet you, {name}!")
